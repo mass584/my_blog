f1d4b397cfee596a68d653ff932ca4901971573c
yoshimitsu.masugata <msgtysmt@gmail.com>
2020-07-07 01:23:06 +0900

    記事用cssの追加

--- wordpress/theme/single.php
+++ wordpress/theme/single.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
   <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri() ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/single.css">
   <?php wp_head(); ?>
 </head>
 <body>
